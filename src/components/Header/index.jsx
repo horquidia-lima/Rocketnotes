8f95c7173e11aa8c5da6cbbfa9e11174a019c764
Horquidia <horquidia.a.lima@gmail.com>
2024-03-11 15:17:14 -0300

    imagem do usuario

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,17 @@
-import {Container} from './styles'
+import {Container, Profile} from './styles'
 
 export function Header(){
     return(
         <Container>
+            <Profile>
+                <img src="https://github.com/horquidia-lima.png" alt="Foto do usuario" />
+
+                <div>
+                    <span>Bem-vindo,</span>
+                    <strong>Horquidia Lima</strong>
+                </div>
+            </Profile>
+
             
         </Container>
     )
